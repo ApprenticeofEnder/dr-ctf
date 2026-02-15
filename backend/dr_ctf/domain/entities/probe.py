a642bb1686ae5e5a7ff619d15a3143745fe5168d
Robert Babaev <github@robertbabaev.tech>
2026-02-15 12:07:25 -0500

    fix(domain/entities/probe): extract non-domain items out
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

--- backend/dr_ctf/domain/entities/probe.py
+++ backend/dr_ctf/domain/entities/probe.py
@@ -1,10 +1,8 @@
 from abc import ABC, abstractmethod
-from http import HTTPStatus
 
 from dr_ctf.domain.entities.base_object import BaseObject
 from dr_ctf.domain.entities.challenge import Challenge
 from dr_ctf.domain.enumerators.probe_type import ProbeType
-from dr_ctf.domain.value_objects.port import Port
 
 
 class Probe(BaseObject, ABC):
@@ -16,22 +14,3 @@ class Probe(BaseObject, ABC):
     @abstractmethod
     def run_check(self):
         pass
-
-
-class HttpProbe(Probe):
-    probe_type: ProbeType = ProbeType.HTTP
-    port: Port
-    endpoint: str
-    expected_status_code: HTTPStatus = HTTPStatus.OK
-
-
-# class TcpProbe(Probe):
-#     pass
-#
-#
-# class DockerProbe(Probe):
-#     pass
-#
-#
-# class CommandProbe(Probe):
-#     pass
