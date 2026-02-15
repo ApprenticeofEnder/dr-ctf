a918c3ed18f95105a031633be876ed025440338d
Robert Babaev <github@robertbabaev.tech>
2026-02-15 13:26:30 -0500

    build: add top level import config
    Signed-off-by: Robert Babaev <github@robertbabaev.tech>

---
+++ backend/dr_ctf/__init__.py
@@ -0,0 +1,6 @@
+from dr_ctf import api as api
+from dr_ctf import application as application
+from dr_ctf import domain as domain
+from dr_ctf import infrastructure as infrastructure
+
+__all__ = ["api", "application", "domain", "infrastructure"]
---
+++ backend/dr_ctf/domain/__init__.py
@@ -0,0 +1,6 @@
+from dr_ctf.domain import entities as entities
+from dr_ctf.domain import enumerators as enumerators
+from dr_ctf.domain import exceptions as exceptions
+from dr_ctf.domain import repositories as repositories
+from dr_ctf.domain import shared as shared
+from dr_ctf.domain import value_objects as value_objects
